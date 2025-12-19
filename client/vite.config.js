4a2fcd1e7094493d57e24115cd220e02bc802685
Roman <ejazahmed4720@gmail.com>
2025-12-19 22:31:28 +0500

    Remove node_modules from repo

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,4 +5,9 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      external: ["fs", "path", "os"], // add the module name from error log if known
+    },
+  },
 });
